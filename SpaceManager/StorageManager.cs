0aee1810374801736359307bb5c1efa4d5107a16
amit-mittal <amit.mittal1993@gmail.com>
2014-04-02 21:48:23 +0530

    First Commit

---
+++ SpaceManager/StorageManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+/**
+ * Interacts with the OS File System
+ */
+namespace RDBMS.SpaceManager{
+	class StorageManager{
+		public void CreateFile(String fileName, int recordLength){
+			try {
+				File.Create(fileName);
+				File.WriteAllBytes(fileName, BitConverter.GetBytes(recordLength));
+			} catch(Exception e) {
+				throw e;
+			}
+		}
+
+		public void DropFile(String fileName){
+			try {
+				File.Delete(fileName);
+			} catch(Exception e){
+				throw e;
+			}
+		}
+
+		public void CreateFolder(String fileName){
+			try{
+				File.Delete(fileName);
+			} catch (Exception e){
+				throw e;
+			}
+		}
+	}
+}

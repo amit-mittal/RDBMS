5e533acb62c5e94568cd63710e766ef02097c6dc
amit-mittal <amit.mittal1993@gmail.com>
2014-04-13 17:26:04 +0530

    Comparison Operators Implemented

--- DataStructure/Condition.cs
+++ DataStructure/Condition.cs
@@ -1,4 +1,6 @@
-﻿
+﻿using System;
+using RDBMS.Util;
+
 namespace RDBMS.DataStructure
 {
 	class Condition
@@ -14,13 +16,155 @@ namespace RDBMS.DataStructure
 
 		public Column Attribute;
 		public ConditionType Sign;
-		public int Value;//TODO think if add value or attribute
+		public String Value;
+		//TODO if we want to add an attribute then make another condition type object
 
-		public Condition(Column attribute, ConditionType sign, int value)
+		public Condition(Column attribute, ConditionType sign, String value)
 		{
 			Attribute = attribute;
 			Sign = sign;
 			Value = value;
+		}
+
+		/**
+		 * Compare 2 integers
+		 * @returns bool
+		 * true - if condition is satisfied
+		 * else false
+		 */
+		public bool CompareIntegers(int v1, int v2, ConditionType sign)
+		{
+			switch (sign)
+			{
+				case ConditionType.Equal:
+					{
+						if (v1 == v2)
+							return true;
+						return false;
+					}
+				case ConditionType.Less:
+					{
+						if (v1 < v2)
+							return true;
+						return false;
+					}
+				case ConditionType.Greater:
+					{
+						if (v1 > v2)
+							return true;
+						return false;
+					}
+				case ConditionType.LessEqual:
+					{
+						if (v1 <= v2)
+							return true;
+						return false;
+					}
+				case ConditionType.GreaterEqual:
+					{
+						if (v1 >= v2)
+							return true;
+						return false;
+					}
+				default:
+					{
+						return false;
+					}
+			}
+		}
+
+		/**
+		 * Compare 2 decimal no.s
+		 * @returns bool
+		 * true - if condition is satisfied
+		 * else false
+		 */
+		public bool CompareDoubles(double v1, double v2, ConditionType sign)
+		{
+			switch (sign)
+			{
+				case ConditionType.Equal:
+					{
+						if (Math.Abs(v1 - v2) <= Constants.Epsilon)
+							return true;
+						return false;
+					}
+				case ConditionType.Less:
+					{
+						if (v1 < v2)
+							return true;
+						return false;
+					}
+				case ConditionType.Greater:
+					{
+						if (v1 > v2)
+							return true;
+						return false;
+					}
+				case ConditionType.LessEqual:
+					{
+						if (v1 < v2 || Math.Abs(v1 - v2) <= Constants.Epsilon)
+							return true;
+						return false;
+					}
+				case ConditionType.GreaterEqual:
+					{
+						if (v1 > v2 || Math.Abs(v1 - v2) <= Constants.Epsilon)
+							return true;
+						return false;
+					}
+				default:
+					{
+						return false;
+					}
+			}
+		}
+
+		/**
+		 * Compare 2 strings
+		 * @returns bool
+		 * true - if condition is satisfied
+		 * else false
+		 */
+		public bool CompareStrings(String s1, String s2, ConditionType sign)
+		{
+			switch (sign)
+			{
+				case ConditionType.Equal:
+					{
+						if (String.Compare(s1, s2) == 0)
+							return true;
+						return false;
+					}
+				case ConditionType.Less:
+					{
+						if (String.Compare(s1, s2) < 0)
+							return true;
+						return false;
+					}
+				case ConditionType.Greater:
+					{
+						if (String.Compare(s1, s2) > 0)
+							return true;
+						return false;
+					}
+				case ConditionType.LessEqual:
+					{
+						if (String.Compare(s1, s2) <= 0)
+							return true;
+						return false;
+					}
+				case ConditionType.GreaterEqual:
+					{
+						if (String.Compare(s1, s2) >= 0)
+							return true;
+						return false;
+					}
+				default:
+					{
+						return false;
+					}
+			}
 		}
 	}
 }
--- Util/Constants.cs
+++ Util/Constants.cs
@@ -8,5 +8,9 @@ namespace RDBMS.Util
 	{
 		public static readonly int MaxStringSize = 50;//size as converting to fixed string length
 		public const int MaxStringLength = 50;
+
+		public const int MaxSelectRecords = 5;
+
+		public const double Epsilon = 0.000001;
 	}
 }
